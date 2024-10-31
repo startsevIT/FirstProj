3256259c5e60ec4c1bcfa82daccab48dfb0a9b53
Кирилл <s.t.a.r.t.s.e.v.it@gmail.com>
2024-10-31 16:14:08 +0500

    Добавлены CRUD-операции

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,6 +7,22 @@ app.MapGet("/", () => "Hello World!");
 
 app.Run();
 
+List<User> GetUsers() => repo;
+void CreateUser(User user) => repo.Add(user);
+User FindUser(int id) => repo.Find(x => x.Id == id);
+void UpdateUser(User dto)
+{
+    foreach (var u in repo)
+        if (u.Id == dto.Id)
+        {
+            if(u.Login != dto.Login && dto.Login != "")
+                u.Login = dto.Login;
+            if(u.Password != dto.Password && dto.Password != "")
+                u.Password = dto.Password;
+        }
+}       
+void DeleteUser(int id) => repo.Remove(FindUser(id));
+
 class User
 {
     public int Id { get; set; } 
